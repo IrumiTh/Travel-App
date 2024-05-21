afd4489d67ac6b60168c7ea425fed16f5c9e8208
irumi <irumiabeywickrama@gmail.com>
2024-05-21 23:15:47 +0530

    Go to detail page when the picture is clicked

--- lib/cubit/app_cubit_logics.dart
+++ lib/cubit/app_cubit_logics.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_app/cubit/app_cubit.dart';
 import 'package:travel_app/cubit/app_cubit_states.dart';
+import 'package:travel_app/pages/detail_page.dart';
 import 'package:travel_app/pages/home_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
 
@@ -20,6 +21,9 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
         builder: (context, state) {
           if (state is WelcomeState) {
             return WelcomePage();
+          }
+          if (state is DetailState) {
+            return DetailPage();
           }
           if (state is LoadedState) {
             return HomePage();
